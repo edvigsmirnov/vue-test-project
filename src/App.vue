38f370381a2d20f626c9f8710f0cc120739d95b4
edvigsmirnov <smirnov.edvig@yandex.ru>
2022-03-18 23:42:01 +1000

    First day of learning Vue(App, Components, Props, Binding)

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,53 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="app">
+    <post-form/>
+    <post-list :posts="posts"/>
+  </div>
 </template>
 
+<script>
+import PostList from "@/components/PostList";
+import PostForm from "@/components/PostForm";
+
+export default {
+  components: {
+    PostList, PostForm,
+  },
+  name: "App",
+  data() {
+    return {
+      posts: [
+        {id: 1, title: 'Post Title', body: 'Body text'},
+        {id: 2, title: 'Post Title 2', body: 'Body text 2'},
+        {id: 3, title: 'Post Title 3', body: 'Body text 3'},
+      ],
+      title: '',
+      body: '',
+    }
+  },
+  methods: {
+    createPost() {
+      const newPost = {
+        id: Date.now(),
+        title: this.title,
+        body: this.body,
+      }
+      this.posts.push(newPost);
+      this.title = '';
+      this.body = '';
+    },
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+.app {
+  padding: 20px;
 }
 </style>
---
+++ src/components/PostForm.vue
@@ -0,0 +1,45 @@
+<template>
+  <form @submit.prevent>
+    <h4>Create new post</h4>
+    <input
+        v-bind:value="title"
+        @input="title = $event.target.value"
+        class="input"
+        type="text"
+        placeholder="Post name">
+    <input
+        v-bind:value="body"
+        @input="body = $event.target.value"
+        class="input"
+        type="text"
+        placeholder="Post description">
+    <button class="btn">Add post</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "PostForm"
+}
+</script>
+
+<style scoped>
+.input {
+  width: 100%;
+  border: 1px solid teal;
+  padding: 15px 15px;
+  margin-top: 15px;
+}
+.btn {
+  align-self: flex-start;
+  margin-top: 15px;
+  padding: 10px 15px;
+  background: none;
+  color: teal;
+  border: 1px solid teal;
+}
+form {
+  display: flex;
+  flex-direction: column;
+}
+</style>
